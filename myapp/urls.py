2741633122c96d479fa9d169274f8b6646d3c764
Bhanuprasad48 <banu.koyalakonda@gmail.com>
2025-07-24 22:33:03 +0530

    login choice update 03

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
      path('', views.loginChoice, name='home'),         # root URL
     
-    path('login/',views.login,name='home'),
+    path('customerLogin/',views.login,name='home'),
     path('newRegisterForm/',views.newRegisterForm,name='New_registration_form'),
     path('newRegistration/',views.newRegistration,name='New_Registration'),
      path('show-users/', views.show_users, name='show_users'),
